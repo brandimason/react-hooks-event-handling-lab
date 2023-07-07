ab613a218a4cf040269e541d10925eefd58dc0d4
Brandi L Mason <masonbrandi66@gmail.com>
2023-07-07 11:21:23 -0600

    event handler lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function handleClick(event) {
+        console.log(event)
+    }
+    return (
+        <div>
+            <button onClick={handleClick} 
+            onBlur={(e) =>
+                    console.log('Hey! Eyes on me!')}
+            onFocus={(e) => 
+                    console.log('Good!')}
+            >Eyes on me</button>
+            {/* <input 
+                onFocus={(e) => 
+                console.log('Good!')}
+            /> */}
+        </div>
+    )
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...", `${event.target.name}: ${event.target.value}`)
+    }
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password"
+            name="search"
+            onChange={handleChange}
+            placeholder="Enter Search Term"
+            />
+        </div>
     )
 }
+
+
 
 export default Keypad;
